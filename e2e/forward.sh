bcbddc1d75482acdfac0f0e96c6f3ca03a9ceadf
smallinsky <m.smallinsky@gmail.com>
2019-04-22 21:01:04 +0200

    Use iptables to forward http trafic to host

---
+++ e2e/forward.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -e 
+
+DOCKER_HOST=$(nslookup host.docker.internal | grep Address | cut -d":" -f2 | tr -d " ")
+echo "Forwarding http and https traffic to ${DOCKER_HOST}"
+
+iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination ${DOCKER_HOST}:8080
+iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination ${DOCKER_HOST}:8443
+
+echo "Running SUT"
+./sut
+
