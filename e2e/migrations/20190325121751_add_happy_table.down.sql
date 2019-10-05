a278eab1d9236f803f2af2931db3dbe18102ee53
Marek Smolinski <marek.smolinski@shipwallet.com>
2019-10-05 16:57:51 +0200

    Rm e2e dir

--- e2e/migrations/20190325121751_add_happy_table.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS `happy_table`;
--- e2e/migrations/20190325121751_add_happy_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE happy_table (
-  query VARCHAR(64) NOT NULL,
-  punchline VARCHAR(64) NOT NULL,
-  PRIMARY KEY (query)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO happy_table (query, punchline) VALUES ("the dirty fork", "Lucky we didn't say anything about the dirty knife");
