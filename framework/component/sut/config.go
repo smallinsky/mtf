61662672ff3bfba841ca4a72c70e335c513d3344
Tobiasz Heller <tobiasz@ingrid.com>
2021-01-07 08:00:56 +0100

    feat: pass mounts

--- framework/component/sut/config.go
+++ framework/component/sut/config.go
@@ -22,6 +22,9 @@ type SutConfig struct {
 	// ExposedPorts is a list of port that will be exposed and forwarded
 	// to docker host.
 	ExposedPorts []int
+	// Mounts allows to pass list of directories which should be mountet
+	// inside system under test container in form 'src:dst'.
+	Mounts []string
 	// RuntimeTypeCommand allows to distinguish between service and simple command binary.
 	RuntimeTypeCommand bool
 
@@ -78,22 +81,45 @@ func BuildContainerConfig(config SutConfig) (*docker.ContainerConfig, error) {
 		Source: config.absoltePath,
 		Target: "/component",
 	}
+
+	customMounts := []docker.Mount{}
+	for _, m := range config.Mounts {
+		src, dst, err := parseMount(m)
+		if err != nil {
+			return nil, err
+		}
+		customMounts = append(customMounts, docker.Mount{
+			Source: src,
+			Target: dst,
+		})
+	}
+
 	var waitPolicy docker.WaitPolicy
 	if !config.RuntimeTypeCommand {
 		waitPolicy = &docker.WaitForProcess{Process: config.binaryName}
 	}
 
+	mounts := []docker.Mount{
+		certMount,
+		binaryMount,
+	}
+	mounts = append(mounts, customMounts...)
 	return &docker.ContainerConfig{
-		Name:  name,
-		Image: image,
-		Env:   env,
-		Mounts: docker.Mounts{
-			certMount,
-			binaryMount,
-		},
+		Name:        name,
+		Image:       image,
+		Env:         env,
+		Mounts:      docker.Mounts(mounts),
 		PortMap:     ports,
 		NetworkName: network,
 		Privileged:  true,
 		WaitPolicy:  waitPolicy,
 	}, nil
+}
+
+func parseMount(in string) (source, destination string, err error) {
+	sd := strings.Split(in, ":")
+	if len(sd) == 2 {
+		return sd[0], sd[1], nil
+	}
+	return "", "", fmt.Errorf("invalid mount format: got %s, expected <src>:<dst>", in)
 }
